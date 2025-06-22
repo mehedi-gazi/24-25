dcc00cd0fb3d698ad78bfc171399e1b595c9edab
Mehedi Gazi <71560571+mehedi-gazi@users.noreply.github.com>
2025-06-22 23:12:47 +0100

    JS Projects inc basics-js

---
+++ js-projects/basics-js/show-hide-pass/app.js
@@ -0,0 +1,15 @@
+// Show and Hide Password Feature via JS
+
+const input = document.querySelector("input");
+const eye = document.querySelector("#eye-icon");
+
+eye.addEventListener("click", () => {
+  //   if (input.type === "password") {
+  //     input.type = "text";
+  //   } else {
+  //     input.type = "password";
+  //   }
+
+  // REFACTOR
+  input.type === "password" ? (input.type = "text") : (input.type = "password");
+});
